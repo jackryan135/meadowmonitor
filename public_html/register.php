9ec3968ccd36bbd352b4428eadf1c505e8a5b31c
Jack Ryan <jackryan135@gmail.com>
2020-03-10 10:26:34 -0700

    changing viewport register

--- public_html/register.php
+++ public_html/register.php
@@ -95,6 +95,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <head>
 	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 	<title>Sign Up</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 	<link href="https://fonts.googleapis.com/css?family=Amatic+SC&display=swap" rel="stylesheet">
