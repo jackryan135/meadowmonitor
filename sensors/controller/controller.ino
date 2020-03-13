595de08def2052367bb52551972347847dca3dee
mdallow97 <mikedallow@gmail.com>
2020-03-12 23:03:09 -0700

    Updated pin numbers and heat is now working

--- sensors/controller/controller.ino
+++ sensors/controller/controller.ino
@@ -2,22 +2,30 @@
 #include "Adafruit_seesaw.h"
 
 // timing
-#define K 10000 // Check sensors every 10 seconds
-#define N 3600000 // Update ESP every 1hr
-#define M 600000 // Update server every 10 min
+#define N 10000 // Update ESP every 10 seconds
+#define M 5000 // Update server every 5 seconds
 #define WATER_TIME 2000
 
-// TODO: change below to real defaults
-#define TEMP 50 // Default temperature
-#define MOISTURE 400 // Default moisture
-#define LIGHT 0 // Default light
+// Default values
+#define TEMP 72 // Default temperature
+#define MOISTURE 2900 // Default moisture
+#define LIGHT 2500 // Default light; TODO: find default for light
+
+// Sensor ranges
+#define MOIST_HIGH 3050
+#define MOIST_MED 2900
+#define MOIST_LOW 2750
+
+#define LIGHT_HIGH 3800
+#define LIGHT_MED 3300
+#define LIGHT_LOW 2800
 
 // inputs
 #define MOISTURE_PIN 36
-#define LIGHT_PIN 0
+#define LIGHT_PIN 15
 
 // outputs
-#define HEAT_PIN 34
+#define HEAT_PIN 2
 #define WATER_EN 35
 #define WATER_PIN1 32
 #define WATER_PIN2 33
@@ -27,14 +35,13 @@ float readTemp(int n);
 
 Adafruit_seesaw ss;
 
-unsigned long k_time;
 unsigned long n_time;
 unsigned long m_time;
 
 struct preferences {
   char dev_id[100];
   float temperature;
-  uint16_t moisture;
+  int moisture;
   int light;
 } user_prefs;
 
@@ -58,7 +65,6 @@ void setup() {
     Serial.println(ss.getVersion(), HEX);
   }
 
-  k_time = millis();
   n_time = millis();
   m_time = millis();
 
@@ -70,15 +76,33 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   unsigned long current_time = millis();
+  if (current_time - n_time >= N) {
+    // Update ESP from server
+    // => struct user_prefs
+    // Data will come back as JSON
+    // Some values will be LOW-HIGH -> convert to analog values via sensor ranges
+    n_time = current_time;
+  }
 
-  if (current_time - k_time >= K) {
+  
+  if (current_time - m_time >= M) {
+    // Update server
+    m_time = current_time;
+    
     // Read sensors, use actuators as necessary
-    float temp = readTemp(20);
-    int moisture = readMoisture(20);
+    float temp = (readTemp(20) * (9.0/5.0)) + 32.0;
+    int moisture = analogRead(MOISTURE_PIN);
+    int light = analogRead(LIGHT_PIN);
 
-    if (temp < user_prefs.temperature)
+
+    if (temp < user_prefs.temperature){
+      Serial.println("heat: HIGH");
       digitalWrite(HEAT_PIN, HIGH);
-    else digitalWrite(HEAT_PIN, LOW);
+    }
+    else {
+      Serial.println("heat: LOW");
+      digitalWrite(HEAT_PIN, LOW);
+    }
 
     if (moisture < user_prefs.moisture) {
       unsigned long start_time = millis();
@@ -86,32 +110,39 @@ void loop() {
       digitalWrite(WATER_PIN1, LOW);
       digitalWrite(WATER_PIN2, HIGH);
       
+      current_time = millis();
       while (current_time - start_time < WATER_TIME){
         current_time = millis();
       }
+      
       digitalWrite(WATER_EN, LOW);
       digitalWrite(WATER_PIN1, LOW);
       digitalWrite(WATER_PIN2, LOW);
         
     }
+    Serial.print("Temperature (F): ");
+    Serial.println(temp);
+
+    // Send moisture, temp, and light to server
+    char *moisture_str;
+
+    if (moisture > MOIST_HIGH)
+      moisture_str = "HIGH";
+    else if (moisture < MOIST_LOW)
+      moisture_str = "LOW";
+    else moisture_str = "MEDIUM";
+
+    char *light_str;
+    if (light > LIGHT_HIGH)
+      light_str = "HIGH";
+    else if (light < LIGHT_LOW)
+      light_str = "LOW";
+    else light_str = "MEDIUM";
       
   }
 
-  if (current_time - n_time >= N) {
-    // Update ESP from server
-    // => struct user_prefs
-    n_time = current_time;
-  }
 
-  if (current_time - m_time >= M) {
-    // Update server
-    m_time = current_time;
-
-    // Transmit these readings to server
-    float temp = readTemp(20);
-    uint16_t moisture = readMoisture(20);
-    // TODO: light??
-  }
+  
 }
 
 float readTemp(int n) {
@@ -126,18 +157,4 @@ float readTemp(int n) {
   }
 
   return (tempSum / (float) n);
-}
-
-int readMoisture(int n) {
-  int moistSum;
-  for (int i = 0; i < n; i++) {
-    int moist = analogRead(MOISTURE_PIN);
-    if (moist == 0) {
-      i--;
-    } else {
-      moistSum += moist;
-    }
-  }
-
-  return (moistSum / n);
 }
--- sensors/heat_test/heat_test.ino
+++ sensors/heat_test/heat_test.ino
@@ -1,16 +1,17 @@
-#define HEAT_PIN 34
+#define HEAT_PIN 2
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   pinMode(HEAT_PIN, OUTPUT);
+  digitalWrite(HEAT_PIN, LOW);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  pinMode(HEAT_PIN, HIGH);
-  delay(2000);
-  pinMode(HEAT_PIN, LOW);
-
-  delay(10000);
+  digitalWrite(HEAT_PIN, HIGH);
+  delay(120000);
+  digitalWrite(HEAT_PIN, LOW);
+  delay(120000);
 }
